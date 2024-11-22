678efc292829ddf1180240f626a644758dd15bcd
Aref Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2024-11-22 11:00:58 +0100

    saved tasks to localstorage

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -16,15 +16,12 @@ export class TasksComponent {
   @Input({ required: true }) name!: string;
   isAddingTask = false;
 
-  constructor(private tasksService: TasksService) {
 
-  }
+  constructor(private tasksService: TasksService) {}
 
   get selectedUserTasks() {
     return this.tasksService.getUserTasks(this.userId);
   }
-
-
 
   onStartAddTask() {
     this.isAddingTask = true;
